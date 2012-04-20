1b7811343d16d20fc7f3a77efae5b10f01d38139
math3os <mgagnon303@gmail.com>
2012-04-19 22:04:01 -0300

    bon... PosIsHeavier()

--- travailpratique2/Passager.java
+++ travailpratique2/Passager.java
@@ -9,6 +9,9 @@ public class Passager {
     private boolean affaire;        //true si affaire
     private char siege;             //#de siege (a to f
     private int nWeight;            //le poid du nom (pour le triage)
+    private int range;
+    private int siegeW;
+    
 
     public Passager(String nom, String prenom, int numPass, String numBill, boolean affaire) {
         this.nom = nom;
@@ -16,6 +19,9 @@ public class Passager {
         this.numPass = numPass;
         this.numBill = numBill;
         this.affaire = affaire;
+        this.siegeW = Character.getNumericValue(numBill.charAt(numBill.length() - 1));
+        this.range =  getRange();
+        
     }
 
     public boolean isAffaire() {
@@ -28,18 +34,21 @@ public class Passager {
     }
 
     public int getRange() {
-        String s = new String();
-        int i = 0;
+       if(!(this.range > 0)){
+            String s = new String();
+            int i = 0;
 
-        while (Character.isDigit(numBill.charAt(i))) {
-            s += numBill.charAt(i);
-            i++;
+            while (Character.isDigit(numBill.charAt(i))) {
+                s += numBill.charAt(i);
+                i++;
+            }
+            return Integer.parseInt(s);
         }
-        return Integer.parseInt(s);
+       else return range;
     }
 
     public int getSiegeW() {  // retourne le poid ASCII du numero de siege
-        return Character.getNumericValue(numBill.charAt(numBill.length() - 2));
+        return siegeW;
     }
 
     public String getLastName() {
@@ -49,7 +58,17 @@ public class Passager {
     public String getFirstName() {
         return prenom;
     }
-
+public boolean posIsHeavier(Passager test){
+    if(this.range == test.getRange()){
+        if(this.siegeW < test.getSiegeW())
+            return false;
+        else return true;
+    }
+    else if (this.range < test.getRange())
+        return false;
+    else return true;
+}
+    
     public boolean isHeavier(Passager name) { //calcul le poid du nom selon un calcul
         if (this.nom.compareTo(name.getLastName()) == 0) {
             if (prenom.compareTo(name.getFirstName()) > 0) {
